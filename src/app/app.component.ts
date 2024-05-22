615418366d185a7a80e62d3731f5bd8b886d3464
mvialo <manuella.vialo@outlook.fr>
2024-05-22 12:44:16 +0200

    Initialisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'challenge1';
+  title = 'Bienvenue sur le site de Manuella la Meilleure';
 }
